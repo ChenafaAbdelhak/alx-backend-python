fa9b11b1afac653003ad8c5e71487cbfbd24bc79
hr <abdelhak.chenafa@univ-temcen.dz>
2024-08-15 13:26:01 +0100

    task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List, Union
+"""module to sum a list of mixed elements"""
+
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """sum a list of mixed elements"""
+    return sum(mxd_list)
