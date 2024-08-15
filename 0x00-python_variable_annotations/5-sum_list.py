ae9b5ed6507cdc7bf4247e8623551a53616bd6ab
hr <abdelhak.chenafa@univ-temcen.dz>
2024-08-15 12:55:41 +0100

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List
+"""module the define a function to sum elements of a list of flots"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """function to sum a list of floats"""
+    return sum(input_list)
