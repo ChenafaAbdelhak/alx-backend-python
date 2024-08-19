a43730342a04d4b75260d70c3fa3fdf945537f97
hr <abdelhak.chenafa@univ-temcen.dz>
2024-08-19 20:48:43 +0100

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import random, asyncio, sys
+"""module thats waits for a random delay"""
+
+async def wait_random(max_delay:int = 10) -> float:
+    """function that sleeps for random"""
+    random_number = random.uniform(0.0, max_delay)
+    await asyncio.sleep(random_number)
+    return random_number
